cd75c414429a0fd34148fca398b87cde158d1f4c
Lucas Anderson <lucasanderson1999@gmail.com>
2025-06-16 21:05:00 -0700

    add toggle, remove unused deps

--- CollisionGlitchRemover.cs
+++ CollisionGlitchRemover.cs
@@ -3,34 +3,39 @@ using HutongGames.PlayMaker.Actions;
 using Modding;
 
 namespace CollisionGlitchRemover {
-    public class CollisionGlitchRemover : Mod {
+    public class CollisionGlitchRemover : Mod, ITogglableMod {
         public static CollisionGlitchRemover instance;
+        public bool enabled = false;
 
         public CollisionGlitchRemover() : base("Collision Glitch Remover") => instance = this;
 
         public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
 
-        public bool ToggleButtonInsideMenu => false;
-
         public override void Initialize() {
             Log("Initializing");
 
+            enabled = true;
             On.HutongGames.PlayMaker.Actions.CheckCollisionSide.OnExit += OnCheckCollisionSideExit;
 
             Log("Initialized");
         }
+
+        public void Unload() => enabled = false;
 
         /*
         The CheckCollisionSide action does not properly reset itself after leaving the midair state at times,
         so the next time the midair state is entered, it immediately triggers the collision transition instead of
         waiting for an actual collision.
 
-        Credit to shownyoung for finding this bug.
+        Credit to shownyoung for finding the cause of this bug.
         */
-        public void OnCheckCollisionSideExit(On.HutongGames.PlayMaker.Actions.CheckCollisionSide.orig_OnExit orig, CheckCollisionSide self) {
+        public void OnCheckCollisionSideExit(On.HutongGames.PlayMaker.Actions.CheckCollisionSide.orig_OnExit orig, CheckCollisionSide self)
+        {
             orig(self);
 
-            GameManager.instance.StartCoroutine(ClearCollisions(self));
+            if (enabled) {
+                GameManager.instance.StartCoroutine(ClearCollisions(self));
+            }
         }
 
         public IEnumerator ClearCollisions(CheckCollisionSide instance) {
